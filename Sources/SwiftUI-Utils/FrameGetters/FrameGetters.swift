f5e5e6fafe44d3425ba12d530a147cc0359d800b
Hugo Lefrancois <hlefrancois@mirego.com>
2024-01-30 11:19:10 -0500

    Add binding in frame getters methods

--- Sources/SwiftUI-Utils/FrameGetters/FrameGetters.swift
+++ Sources/SwiftUI-Utils/FrameGetters/FrameGetters.swift
@@ -1,29 +1,57 @@
 import SwiftUI
 
 public extension View {
+    func readSize(_ frame: Binding<CGSize>) -> some View {
+        background(FrameGetter(coordinateSpace: .local))
+            .onPreferenceChange(FramePreferenceKey.self) {
+                frame.wrappedValue = $0.size
+            }
+    }
+
+    func readLocalFrame(_ frame: Binding<CGRect>) -> some View {
+        background(FrameGetter(coordinateSpace: .local))
+            .onPreferenceChange(FramePreferenceKey.self) {
+                frame.wrappedValue = $0
+            }
+    }
+
     func readLocalFrame(_ reader: @escaping (CGRect) -> Void) -> some View {
-        background(LocalFrameGetter())
-            .onPreferenceChange(LocalFramePreferenceKey.self) {
+        background(FrameGetter(coordinateSpace: .local))
+            .onPreferenceChange(FramePreferenceKey.self) {
                 reader($0)
             }
     }
 
+    func readGlobalFrame(_ frame: Binding<CGRect>) -> some View {
+        background(FrameGetter(coordinateSpace: .global))
+            .onPreferenceChange(FramePreferenceKey.self) {
+                frame.wrappedValue = $0
+            }
+    }
+
     func readGlobalFrame(_ reader: @escaping (CGRect) -> Void) -> some View {
-        background(GlobalFrameReader())
-            .onPreferenceChange(GlobalFramePreferenceKey.self) {
+        background(FrameGetter(coordinateSpace: .global))
+            .onPreferenceChange(FramePreferenceKey.self) {
                 reader($0)
             }
     }
 
     func readNamedFrame(coordinateName: String, _ reader: @escaping (CGRect) -> Void) -> some View {
-        background(NamedFrameReader(coordinateName: coordinateName))
-            .onPreferenceChange(NamedFramePreferenceKey.self) {
+        background(FrameGetter(coordinateSpace: .named(coordinateName)))
+            .onPreferenceChange(FramePreferenceKey.self) {
                 reader($0)
+            }
+    }
+
+    func readNamedFrame(coordinateName: String, _ frame: Binding<CGRect>) -> some View {
+        background(FrameGetter(coordinateSpace: .named(coordinateName)))
+            .onPreferenceChange(FramePreferenceKey.self) {
+                frame.wrappedValue = $0
             }
     }
 }
 
-private struct LocalFramePreferenceKey: PreferenceKey {
+private struct FramePreferenceKey: PreferenceKey {
     static var defaultValue: CGRect = .zero
 
     static func reduce(value: inout CGRect, nextValue: () -> CGRect) { }
@@ -31,48 +59,15 @@ private struct LocalFramePreferenceKey: PreferenceKey {
     typealias Value = CGRect
 }
 
-private struct LocalFrameGetter: View {
+private struct FrameGetter: View {
+    let coordinateSpace: CoordinateSpace
+
     var body: some View {
         GeometryReader { geometry in
-            Color.clear.preference(key: LocalFramePreferenceKey.self, value: geometry.frame(in: .local))
-        }
-    }
-}
-
-private struct GlobalFramePreferenceKey: PreferenceKey {
-    static var defaultValue: CGRect = .zero
-
-    static func reduce(value: inout CGRect, nextValue: () -> CGRect) { }
-}
-
-private struct GlobalFrameReader: View {
-    var body: some View {
-        GeometryReader { proxy in
-            Color.clear
-                .preference(
-                    key: GlobalFramePreferenceKey.self,
-                    value: proxy.frame(in: .global)
-                )
-        }
-    }
-}
-
-private struct NamedFramePreferenceKey: PreferenceKey {
-    static var defaultValue: CGRect = .zero
-
-    static func reduce(value: inout CGRect, nextValue: () -> CGRect) { }
-}
-
-private struct NamedFrameReader: View {
-    let coordinateName: String
-
-    var body: some View {
-        GeometryReader { proxy in
-            Color.clear
-                .preference(
-                    key: NamedFramePreferenceKey.self,
-                    value: proxy.frame(in: .named(coordinateName))
-                )
+            Color.clear.preference(
+                key: FramePreferenceKey.self,
+                value: geometry.frame(in: coordinateSpace)
+            )
         }
     }
 }
